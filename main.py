a3e9ece1e047cd9d0305083705b028048420af70
Zigmars Vilcans <zims@inbox.lv>
2021-11-26 08:11:59 +0000

    graphs + slider

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ st.title("1app")
 st.subheader("intro")
 st.write("webapp")
 st.write(df)
+st.line_chart(df)
+st.area_chart(df)
+
+myslider = st.slider("Celsius")
+st.write(myslider, "in F is", myslider * 9/5 + 32)
