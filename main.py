6015bacdaf01257b8aa2dc8ce9510ad1fd16aa6e
Zigmars Vilcans <zims@inbox.lv>
2021-11-26 08:02:08 +0000

    with df

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas
+
+data = {
+  'Series_1': [1, 3, 4, 5, 7],
+  'Series_2': [10, 30, 40, 100, 250]
+}
+
+df = pandas.DateFrame(data)
+
 
 st.title("1app")
 st.subheader("intro")
 st.write("webapp")
+st.write(df)
